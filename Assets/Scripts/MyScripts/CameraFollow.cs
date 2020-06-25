ffd9b99bf95b9e481effb3ae9c6002128d53967a
EvertonBorges <evertonsoaresborges@gmail.com>
2020-06-25 09:23:50 -0400

    refact - refactory CameraFollow script

--- Assets/Scripts/MyScripts/CameraFollow.cs
+++ Assets/Scripts/MyScripts/CameraFollow.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("Target of the camera")]
     private Transform target;
 
     [SerializeField]
     [Range(2f, 5f)]
+    [Tooltip("Camera don´t go below this position")]
     private float maxYPosition;
 
     [SerializeField]
     [Range(1f, 5f)]
+    [Tooltip("Duration of the camera animation")]
     private float animationDuration;
 
+    // To concludes if finished animation
     private float transition = 0f;
 
+    // Addictional position to camera finish the animation and after this it always be in this position
     private Vector3 startOffset;
+
+    // Addictional coordenates to start the camera
     private Vector3 animationOffset = new Vector3(0f, 4f, 5f);
 
-    // Start is called before the first frame update
     void Start()
     {
         startOffset = transform.position - target.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 finishPosition = target.position + startOffset;
